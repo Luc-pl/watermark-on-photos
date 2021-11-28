d8a9beca11413fc02c5b28cbf21979132028014f
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-11-28 18:07:40 +0100

    Add watermark logo

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const Jimp = require('jimp');
 
-const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
   const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
   await image.quality(100).writeAsync(outputFile);
 };
 
-addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world')
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
